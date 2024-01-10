e2ae7b4874478076f3fef42fb4dd80f6ae012986
labeneko <labecat@gmail.com>
2024-01-10 18:15:32 +0900

    ファイルわけ

---
+++ index.js
@@ -0,0 +1,10 @@
+const h1 = document.createElement('h1');
+h1.innerText = 'ようこそPlaywrightテストページへ';
+document.body.appendChild(h1);
+const h2 = document.createElement('h2');
+h2.innerText = 'おすすめサイトのリンク';
+document.body.appendChild(h2);
+const ul = document.createElement('ul');
+ul.innerHTML = `<li><a href="https://www.nnn.ed.nico/">N予備校</a></li>
+    <li><a href="https://zen-univ.jp/">ZEN大学</a></li>`
+document.body.appendChild(ul);
